ac3712e5cb6df2931c5a170807cf6a60d2056f28
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-10-11 22:12:31 +0700

    perbaiki css navbar

--- resources/views/layouts/dashboard/page-layout.blade.php
+++ resources/views/layouts/dashboard/page-layout.blade.php
@@ -10,9 +10,9 @@
 <body class="bg-gray-50">
     <div id="app" class="h-screen flex flex-col">
         @include('layouts.dashboard.component-sidebar')
-        <div class="md:ml-64 overflow-auto">
+        <div class="md:ml-64 flex flex-col h-full">
             @include('layouts.dashboard.component-navbar')
-            <div class="container pt-8">
+            <div class="container pt-8 h-full overflow-auto">
                 @yield('pages')
             </div>
         </div>
