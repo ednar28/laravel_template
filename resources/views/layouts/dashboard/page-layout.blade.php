feaa23c8e271c54c16562f296ca02c1a5b591011
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-10-12 20:42:49 +0700

    tambah alpine js

--- resources/views/layouts/dashboard/page-layout.blade.php
+++ resources/views/layouts/dashboard/page-layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>{{ config('app.name') }}</title>
 </head>
 
