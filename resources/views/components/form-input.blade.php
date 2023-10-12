69f5dadf76536b2d279196296f64b7e265a462fa
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-10-12 22:27:15 +0700

    form input bisa masukkan value

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -3,7 +3,7 @@
         {{ $label }}
     </label>
 
-    <input type="{{$type}}" class="form-input @error($name) error @enderror" name="{{$name}}"
+    <input type="{{$type}}" class="form-input @error($name) error @enderror" name="{{$name}}" value="{{$value}}"
         placeholder="{{$placeholder}}">
 
     @error($name)
