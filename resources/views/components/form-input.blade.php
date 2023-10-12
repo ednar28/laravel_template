60d940989fe7fa61b0600180c7fe911566188a28
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-10-12 20:41:17 +0700

    buat template  button dan form

---
+++ resources/views/components/form-input.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <label for="{{$id}}" class="form-label">
+        {{ $label }}
+    </label>
+
+    <input type="{{$type}}" class="form-input @error($name) error @enderror" name="{{$name}}"
+        placeholder="{{$placeholder}}">
+
+    @error($name)
+    <div class="form-error">
+        {{$message}}
+    </div>
+    @enderror
+</div>
