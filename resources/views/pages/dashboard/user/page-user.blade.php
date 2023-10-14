95c09b6268291c80dcd67a511c8a5fb4802dfe3f
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-10-14 09:03:55 +0700

    Update page-user.blade.php

--- resources/views/pages/dashboard/user/page-user.blade.php
+++ resources/views/pages/dashboard/user/page-user.blade.php
@@ -38,20 +38,7 @@
             </tbody>
         </table>
 
-        <x-modal refs="deleteUser">
-            @section('content')
-            Apakah anda yakin menghapus <span class="text-red-500 font-medium" x-text="deleteUser.name + '?'"></span>
-
-            <form x-bind:action="'{{route('dashboard.user.index') . '/'}}'+deleteUser.id" method="POST">
-                <div class="mt-6 flex justify-center items-center space-x-4">
-                    @method('DELETE')
-                    @csrf
-                    <x-button-submit label="Hapus" />
-                    <button class="btn btn-info w-28" x-on:click="deleteUser = null">Batal</button>
-                </div>
-            </form>
-            @endsection
-        </x-modal>
+        @include('pages.dashboard.user.modal-delete')
     </div>
 
     <div class="w-full">
