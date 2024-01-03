0f2870d6e697c9ddd00d10ff6221b1ab508833f9
aisyahaz00 <ichaaazzahra28@gmail.com>
2024-01-03 20:15:29 +0700

    add authentication

---
+++ app/Http/Controllers/PenjualanController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PenjualanController extends Controller
+{
+    public function penjualan()
+    {
+
+        $vendor = DB::table('penjualan')->get();
+
+
+        return view('pages.dashboard.penjualan.halaman-penjualan');
+    }
+
+    public function formTambah()
+    {
+        return view('pages.dashboard.penjualan.form-tambah');
+    }
+
+    public function tambah(Request $request)
+    {
+        $validated = $request->validate([
+            'barang' => 'required|array',
+            'barang.*.idbarang' => 'required|exists:barang,idbarang',
+            'barang.*.jumlah' => 'required|integer|min:1',
+        ]);
+
+        $idpenjualan = DB::table('penjualan')->insertGetId([
+            'iduser' => auth()->id(),
+            'aub_total' => 0,
+            'ppn' => 0,
+            'total_nilai' => 0,
+            'created_at' => now(),
+        ]);
+
+        $totalPembelian = 0;
+        foreach($validated['barang'] as $formBarang) {
+            $barang = DB::table('barang')->where('idbarang', $formBarang['idbarang'])->first();
+
+            DB::table('penjualan_detail')->insert([
+                'penjualan_idpenjualan' => $idpenjualan,
+                'idbarang' => $formBarang['idbarang'],
+                'harga_satuan' => $barang->harga,
+                'jumlah' => $formBarang['jumlah'],
+                'sub_total' => $subTotal = $barang->harga * $formBarang['jumlah'],
+            ]);
+
+            $totalPembelian += $subTotal;
+        }
+
+        DB::table('penjualan')
+            ->where('idpenjualan', $idpenjualan)
+            ->update([
+                'sub_total' => $totalPembelian,
+                'ppn' => 11,
+                'total_nilai' => $totalPembelian - round(($totalPembelian * 11 / 100)),
+            ]);
+
+        return redirect()
+            ->route('dashboard.penjualan.halamanPenjualan')
+            ->with(['message' => ' Berhasil Simpan ' . $validated['nama_penjualan']]);
+
+    }
+
+
+}
--- database/migrations/2023_12_21_133835_create_penjualan_table.php
+++ database/migrations/2023_12_21_133835_create_penjualan_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,8 @@ return new class extends Migration
         Schema::create('penjualan', function (Blueprint $table) {
             $table->id('idpenjualan');
             $table->foreignId('iduser')->constrained('users', 'iduser');
+            $table->integer('sub_total');
+            $table->integer('ppn');
             $table->integer('total_nilai');
             $table->timestamps();
         });
